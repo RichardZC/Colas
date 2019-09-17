9c1adc0cd59b33a1d2d2a721cf43b9f242031820
yovani <ycarbajalo@gmail.com>
2019-09-18 00:02:09 +0200

    19092019

---
+++ Hra.Colas.Totem/TotenAdmisionForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Hra.Colas.Totem
+{
+    partial class TotenAdmisionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TotenAdmisionForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(188, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(394, 126);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Location = new System.Drawing.Point(76, 157);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(676, 283);
+            this.panel1.TabIndex = 4;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(394, 50);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(226, 190);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "opcion2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Visible = false;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(37, 50);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(201, 190);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "opcion1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCerrar.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btnCerrar.Enabled = false;
+            this.btnCerrar.Location = new System.Drawing.Point(741, 0);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(46, 27);
+            this.btnCerrar.TabIndex = 4;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.UseWaitCursor = true;
+            this.btnCerrar.Visible = false;
+            this.btnCerrar.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // TotenAdmisionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 462);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TotenAdmisionForm";
+            this.UseWaitCursor = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.TotenAdmisionForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+    }
+}
---
+++ Hra.Colas.Totem/TotenAdmisionForm.cs
@@ -0,0 +1,82 @@
+﻿using Hra.Colas.Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hra.Colas.Totem
+{
+    public partial class TotenAdmisionForm : Form
+    {
+        public TotenAdmisionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TotenAdmisionForm_Load(object sender, EventArgs e)
+        {
+            var bloque = int.Parse(ConfigurationManager.AppSettings["Bloque"]);
+            var servicios = ServicioBL.Listar(x => x.BloqueId == bloque); 
+
+            var i = servicios.Count;
+
+            if (i==1)
+            {
+                btn1.Visible = true;
+                btn1.Text = servicios[0].Denominacion;
+            }
+            if (i == 2)
+            {
+                btn1.Visible = true;
+                btn1.Text = servicios[0].Denominacion;
+
+                btn2.Visible = true;
+                btn2.Text = servicios[1].Denominacion;
+            }
+            //for (int i = 0; i < servicios.Count; i++)
+            //{
+            //    Button btn1 = new Button();
+            //    btn1.Location = new System.Drawing.Point((i * 350), 60);
+            //    btn1.Size = new System.Drawing.Size(200, 25);
+            //    btn1.Text = servicios[i].Denominacion;
+            //    btn1.Click += new EventHandler(button1_Click);
+
+            //    panel1.Controls.Add(btn1);
+            //}
+
+
+
+        }
+        //private void button1_Click(object sender, System.EventArgs e)
+        //{
+        //    MessageBox.Show("click");
+
+        //}
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
