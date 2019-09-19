b3c50e86060f0ca5d10ee371b3d9216cf6756154
canchari <sabino.cg88@gmail.com>
2019-09-19 09:16:40 -0500

    ventanillas

---
+++ Hra.Colas.Negocio/VentanillaBL.cs
@@ -0,0 +1,13 @@
+﻿using Hra.Colas.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hra.Colas.Negocio
+{
+    public class VentanillaBL : Repositorio<Ventanilla>
+    {
+    }
+}
