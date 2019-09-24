81666e131a879f217243f45a22127023ac6bad6e
canchari <sabino.cg88@gmail.com>
2019-09-24 09:41:31 -0500

    CONFIGURACION VENTANILLAS

--- Hra.Colas.Web/Controllers/VentanillaController.cs
+++ Hra.Colas.Web/Controllers/VentanillaController.cs
@@ -16,5 +16,48 @@ namespace Hra.Colas.Web.Controllers
             return View(VentanillaBL.Listar( includeProperties:"Bloque,Usuario,Servicio" ));
        
         }
+        public ActionResult Mantener(int id = 0)
+        {
+            ViewBag.cboUsuario = new SelectList(UsuarioBL.Listar(null, x => x.OrderByDescending(y => y.Id)), "Id", "Denominacion");
+            ViewBag.cboUsuario = new SelectList(ServicioBL.Listar(null, x => x.OrderByDescending(y => y.Id)), "Id", "Denominacion");
+
+            ViewBag.cboBloque = new SelectList(BloqueBL.Listar(), "Id", "Denominacion");
+            ViewBag.cboUsuario = new SelectList(UsuarioBL.Listar(), "Id", "NombreCompleto");
+            ViewBag.cboServicio = new SelectList(ServicioBL.Listar(), "Id", "Denominacion");
+
+            if (id == 0)
+                return View(new Datos.Ventanilla() {  });
+     
+            else
+                return View(VentanillaBL.Obtener(id));
+
+        }
+
+        [HttpPost]
+        public ActionResult Guardar(Datos.Ventanilla ventanilla, string activo)
+        {
+            var rm = new Comun.ResponseModel();
+          
+            try
+            {
+                if (ventanilla.Id == 0)
+                {
+                    VentanillaBL.Crear(ventanilla);
+                }
+                else
+                {
+                    VentanillaBL.ActualizarParcial(ventanilla, x => x.Denominacion, x => x.BloqueId, x => x.UsuarioId, x => x.ServicioId);
+                }
+                rm.SetResponse(true);
+                rm.href = Url.Action("Index", "Ventanilla");
+
+            }
+            catch (Exception ex)
+            {
+                rm.SetResponse(false, ex.Message);
+            }
+            return Json(rm, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
