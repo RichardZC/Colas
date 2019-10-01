7c54b853314345d55efc4a984983d228ef7e18a8
yovani <ycarbajalo@gmail.com>
2019-10-01 15:24:37 +0200

    totem

--- Hra.Colas.Totem/TotenAdmisionForm.Designer.cs
+++ Hra.Colas.Totem/TotenAdmisionForm.Designer.cs
@@ -28,101 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TotenAdmisionForm));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.btn2 = new System.Windows.Forms.Button();
-            this.btn1 = new System.Windows.Forms.Button();
             this.btnCerrar = new System.Windows.Forms.Button();
+            this.entityCommand1 = new System.Data.Entity.Core.EntityClient.EntityCommand();
+            this.printDocument2 = new System.Drawing.Printing.PrintDocument();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Enabled = false;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(188, 25);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(394, 126);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.UseWaitCursor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.btn2);
-            this.panel1.Controls.Add(this.btn1);
-            this.panel1.Location = new System.Drawing.Point(76, 157);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(676, 283);
-            this.panel1.TabIndex = 4;
-            // 
-            // btn2
-            // 
-            this.btn2.Location = new System.Drawing.Point(394, 50);
-            this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(226, 190);
-            this.btn2.TabIndex = 1;
-            this.btn2.Text = "opcion2";
-            this.btn2.UseVisualStyleBackColor = true;
-            this.btn2.Visible = false;
-            // 
-            // btn1
-            // 
-            this.btn1.Location = new System.Drawing.Point(37, 50);
-            this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(201, 190);
-            this.btn1.TabIndex = 0;
-            this.btn1.Text = "opcion1";
-            this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.Btn1_Click);
             // 
             // btnCerrar
             // 
             this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCerrar.Cursor = System.Windows.Forms.Cursors.WaitCursor;
-            this.btnCerrar.Enabled = false;
-            this.btnCerrar.Location = new System.Drawing.Point(741, 0);
+            this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrar.Location = new System.Drawing.Point(736, 12);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Size = new System.Drawing.Size(46, 27);
             this.btnCerrar.TabIndex = 4;
             this.btnCerrar.Text = "Cerrar";
             this.btnCerrar.UseVisualStyleBackColor = true;
-            this.btnCerrar.UseWaitCursor = true;
-            this.btnCerrar.Visible = false;
-            this.btnCerrar.Click += new System.EventHandler(this.Button2_Click);
+            this.btnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
+            this.btnCerrar.MouseEnter += new System.EventHandler(this.BtnCerrar_MouseEnter);
+            this.btnCerrar.MouseLeave += new System.EventHandler(this.BtnCerrar_MouseLeave);
+            // 
+            // entityCommand1
+            // 
+            this.entityCommand1.CommandTimeout = 0;
+            this.entityCommand1.CommandTree = null;
+            this.entityCommand1.Connection = null;
+            this.entityCommand1.EnablePlanCaching = true;
+            this.entityCommand1.Transaction = null;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::Hra.Colas.Totem.Properties.Resources.fndtoten;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(794, 539);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            
+            // 
+            // btn1
+            // 
+            this.btn1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn1.BackColor = System.Drawing.Color.PeachPuff;
+            this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 41F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btn1.Location = new System.Drawing.Point(3, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(396, 220);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "opcion1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn2.BackColor = System.Drawing.Color.PeachPuff;
+            this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 41F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btn2.Location = new System.Drawing.Point(3, 229);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(396, 221);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "opcion2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btn2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(70, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(402, 453);
+            this.tableLayoutPanel1.TabIndex = 6;
             // 
             // TotenAdmisionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(803, 462);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(794, 539);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.btnCerrar);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "TotenAdmisionForm";
-            this.UseWaitCursor = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.TotenAdmisionForm_Load);
+            this.MouseEnter += new System.EventHandler(this.TotenAdmisionForm_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.TotenAdmisionForm_MouseLeave);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnCerrar;
-        private System.Windows.Forms.Button btn2;
+        private System.Data.Entity.Core.EntityClient.EntityCommand entityCommand1;
+        private System.Drawing.Printing.PrintDocument printDocument2;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- Hra.Colas.Totem/TotenAdmisionForm.cs
+++ Hra.Colas.Totem/TotenAdmisionForm.cs
@@ -1,10 +1,12 @@
-﻿using Hra.Colas.Negocio;
+﻿using Hra.Colas.Datos;
+using Hra.Colas.Negocio;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,69 +16,127 @@ namespace Hra.Colas.Totem
 {
     public partial class TotenAdmisionForm : Form
     {
+
+        string imagen = @"C:\GITHUB\Colas\Hra.Colas.Totem\img\logo.png";
+        public string Servicio { get; set; }
+        public string Codigo { get; set; } 
+        
         public TotenAdmisionForm()
         {
             InitializeComponent();
         }
-
         private void TotenAdmisionForm_Load(object sender, EventArgs e)
         {
             var bloque = int.Parse(ConfigurationManager.AppSettings["Bloque"]);
-            var servicios = ServicioBL.Listar(x => x.BloqueId == bloque); 
-
+            var servicios = ServicioBL.Listar(x => x.BloqueId == bloque);
             var i = servicios.Count;
-
-            if (i==1)
+            
+            if (i == 1)
             {
                 btn1.Visible = true;
                 btn1.Text = servicios[0].Denominacion;
+                btn1.Tag = servicios[0].Id;
+
+                btn2.Visible = false;
             }
             if (i == 2)
             {
                 btn1.Visible = true;
                 btn1.Text = servicios[0].Denominacion;
+                btn1.Tag = servicios[0].Id;
 
                 btn2.Visible = true;
                 btn2.Text = servicios[1].Denominacion;
+                btn2.Tag = servicios[1].Id;
             }
-            //for (int i = 0; i < servicios.Count; i++)
-            //{
-            //    Button btn1 = new Button();
-            //    btn1.Location = new System.Drawing.Point((i * 350), 60);
-            //    btn1.Size = new System.Drawing.Size(200, 25);
-            //    btn1.Text = servicios[i].Denominacion;
-            //    btn1.Click += new EventHandler(button1_Click);
-
-            //    panel1.Controls.Add(btn1);
-            //}
-
-
-
         }
-        //private void button1_Click(object sender, System.EventArgs e)
-        //{
-        //    MessageBox.Show("click");
-
-        //}
-
-        private void Label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Button2_Click(object sender, EventArgs e)
+        private void BtnCerrar_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
-        private void Btn1_Click(object sender, EventArgs e)
+        private void BtnCerrar_MouseEnter(object sender, EventArgs e)
         {
 
+            //btnCerrar.Hide();
+            //BackColor = Color.Red;
         }
+
+        private void BtnCerrar_MouseLeave(object sender, EventArgs e)
+        {
+            //btnCerrar.Show();
+            //BackColor = Color.Black;
+        }
+
+        private void TotenAdmisionForm_MouseEnter(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void TotenAdmisionForm_MouseLeave(object sender, EventArgs e)
+        {
+            //btnCerrar.Hide();
+            //BackColor = Color.Red;
+        }
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            GenerarTikect(int.Parse(btn1.Tag.ToString()), btn1.Text);
+        }
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            GenerarTikect(int.Parse(btn2.Tag.ToString()), btn2.Text);
+        }
+
+        private void GenerarTikect(int servicioId, string servicio)
+        {
+            Servicio = servicio;
+            var numero = ColaBL.Contar(x => x.Fecha.Year == DateTime.Now.Year
+            && x.Fecha.Month == DateTime.Now.Month && x.Fecha.Day == DateTime.Now.Day
+            && x.ServicioId == servicioId);// obtener del servidor
+            Codigo = servicio.Substring(0, 2) + (numero + 1).ToString();
+
+            // mejorar para que guarde la fecha de servidor
+            ColaBL.Crear(new Cola()
+            {
+                ServicioId = servicioId,
+                Fecha = DateTime.Now,
+                Codigo = Codigo
+            });
+
+            printDocument2 = new PrintDocument();
+            PrinterSettings ps = new PrinterSettings();
+            printDocument2.PrinterSettings = ps;
+            printDocument2.PrintPage += Imprimir;
+          
+           
+            var tam = new PaperSize();
+            tam.RawKind = 35;
+            //tam.Height = 56;
+            //tam.Width = 56;
+            ps.DefaultPageSettings.PaperSize = new PaperSize("210 x 297 mm", 150,130);
+            //ps.DefaultPageSettings.PaperSize = tam;
+            //ps.DefaultPageSettings.Landscape = true;
+            //ps.DefaultPageSettings.Margins = new Margins(0, 0, 0, 0);
+
+            // printDocument2.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("ticket", 240, 350);
+            printDocument2.Print();                                          
+        }
+
+        private void Imprimir(Object sender, PrintPageEventArgs e)
+        {            
+            Font font1 = new Font("Arial", 06, FontStyle.Bold, GraphicsUnit.Point);
+            Font font2 = new Font("Arial", 16, FontStyle.Bold, GraphicsUnit.Point);
+            int width = 350;
+            int y = 5;
+            Image img = Image.FromFile(imagen);
+            e.Graphics.DrawImage(img, new Rectangle(50, 7, 150, 30));
+            e.Graphics.DrawString("" + Servicio, font1, Brushes.Black, new RectangleF(20, y += 50, width, 20));
+            e.Graphics.DrawString("Numero de ticket", font1, Brushes.Black, new RectangleF(20, y += 20, 400, 40));
+            e.Graphics.DrawString("" + Codigo, font2, Brushes.Black, new RectangleF(65, y += 10, width, 20));
+            e.Graphics.DrawString("No pierda su Ticket", font1, Brushes.Black, new RectangleF(20, y += 25, 400, 40));
+            e.Graphics.DrawString("" + DateTime.Now.ToString(), font1, Brushes.Black, new RectangleF(85, y += 20, width, 20));
+        }
+
+        
     }
 }
