32b00dc5e63feacd7457e6d7af963b0c97a4941b
Richard Zapata <r_zapatac@hotmail.com>
2019-11-12 12:36:56 -0500

    llamdo

---
+++ Hra.Colas.Web/Controllers/LlamadoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hra.Colas.Web.Controllers
+{
+    public class LlamadoController : Controller
+    {
+        // GET: Llamado
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
